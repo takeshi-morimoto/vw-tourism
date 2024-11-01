e06b1ef707f72e30b99b9a596fa983077a571d67
Takeshi Morimoto <takeshi.morimoto@koikoi.co.jp>
2024-11-01 19:39:00 +0900

    テスト

--- functions.php
+++ functions.php
@@ -365,18 +365,6 @@ function vw_tourism_pro_sanitize_choices( $input, $setting ) {
 	}
 }
 
-/* Excerpt Read more overwrite */
-function vw_tourism_pro_excerpt_more( $link ) {
-	if ( is_admin() ) {
-		return $link;
-	}
-	$link = sprintf( '<p class="link-more"><a href="%1$s" class="more-link">%2$s</a></p>',
-		esc_url( get_permalink( get_the_ID() ) ),
-		/* translators: %s: Name of current post */
-		sprintf( __( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'vw-tourism-pro' ), get_the_title( get_the_ID() ) )
-	);
-	return ' &hellip; ' . $link;
-}
 add_filter( 'excerpt_more', 'vw_tourism_pro_excerpt_more' );
 
 define('vw_tourism_pro_THEME_DOC','https://www.vwthemesdemo.com/docs/vw-tourism-pro/');
