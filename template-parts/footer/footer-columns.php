f3f41bddeef2ac33ce2427871f0bb69be5b289a3
Takeshi Morimoto <takeshi.morimoto@koikoi.co.jp>
2024-12-18 15:09:25 +0900

    test

--- template-parts/footer/footer-columns.php
+++ template-parts/footer/footer-columns.php
@@ -1,69 +1,76 @@
 <?php
-$address_section = get_theme_mod( 'vw_tourism_pro_radiolast_enable' );
-if ( 'Disable' == $address_section ) {
-	return;
+/**
+ * Template part for displaying footer content
+ *
+ * @package vw_tourism_pro
+ */
+
+// セクションの表示/非表示設定
+$address_section = get_theme_mod('vw_tourism_pro_radiolast_enable');
+if ('Disable' == $address_section) {
+    return;
 }
 
-if( get_theme_mod('vw_tourism_pro_section_footer_bgcolor','') ) {
- $footer_backg = 'background-color:'.esc_attr(get_theme_mod('vw_tourism_pro_section_footer_bgcolor','')).';';
-}elseif( get_theme_mod('vw_tourism_pro_footer_bgimage','') ){
- $footer_backg = 'background-image:url(\''.esc_url(get_theme_mod('vw_tourism_pro_footer_bgimage')).'\')';
-}else{
- $footer_backg='';
+// 背景設定
+if ($bgcolor = get_theme_mod('vw_tourism_pro_section_footer_bgcolor', '')) {
+    $footer_backg = 'background-color:' . esc_attr($bgcolor) . ';';
+} elseif ($bgimage = get_theme_mod('vw_tourism_pro_footer_bgimage', '')) {
+    $footer_backg = 'background-image:url(' . esc_url($bgimage) . ');';
+} else {
+    $footer_backg = '';
 }
-$img_bg =get_theme_mod('vw_tourism_pro_section_footer_bg_attachment');
+$img_bg = get_theme_mod('vw_tourism_pro_section_footer_bg_attachment', '');
+
+// ウィジェットカラム数のチェック
+$count = 0;
+$sidebars = ['footer-1', 'footer-2', 'footer-3', 'footer-4'];
+foreach ($sidebars as $sidebar) {
+    if (is_active_sidebar($sidebar)) {
+        $count++;
+    }
+}
+
+// カラムのクラス設定
+switch ($count) {
+    case 1:
+        $col_class = 'col-lg-12 col-sm-6';
+        break;
+    case 2:
+        $col_class = 'col-lg-6 col-sm-6';
+        break;
+    case 3:
+    case 4:
+        $col_class = 'col-lg-4 col-md-4 col-sm-6 pb-3';
+        break;
+    default:
+        $col_class = 'footer_hide';
+        break;
+}
 ?>
-<?php if (get_theme_mod('vw_tourism_pro_custom_footer_widget_section', 'true') !='') { ?>
-<!-- <section id="footer" class="pb-0"> -->
-	<div id="footer-inner" style="<?php echo esc_attr( $footer_backg ); ?>" class="<?php echo esc_attr($img_bg); ?>">
-		<div class="container footer-cols">
-			<?php
-				$count = 0;
-				if ( is_active_sidebar('footer-1') != '' ) {
-					$count++;
-				}
-				if ( is_active_sidebar('footer-2') != '' ) {
-					$count++;
-				}
-				if ( is_active_sidebar('footer-3') != '' ) {
-					$count++;
-				}
-				if ( is_active_sidebar('footer-4') != '' ) {
-					$count++;
-				}
 
-				if ( $count == 1 ) {
-					$colmd = 'col-lg-12 col-sm-6';
-				} elseif ( $count == 2 ) {
-					$colmd = 'col-lg-6 col-sm-6';
-				} elseif ( $count == 3 ){
-					$collg = 'col-lg-4 col-md-4 col-sm-12 pb-sm-3 pb-4';
-					$colmd = 'col-lg-4 col-md-4 col-sm-6 pb-sm-3 pb-4';
-				} else {
-					$collg = 'col-lg-4 col-sm-12';
-					$colmd = 'col-lg-4 col-sm-6';
-				}
-			?>
-			<div class="row">
-				<div class="col-lg-4 col-md-12">
-						<?php get_template_part( 'template-parts/footer/footer-contact' ); ?>
-				</div>
-				<div class="col-lg-8 col-md-12	">
-					<div class="row footer-col-div py-xl-5 py-lg-3">
-						<div class="<?php if ( is_active_sidebar('footer-1') == '' ) { echo 'footer_hide'; } else { echo esc_html( $collg ); } ?> text-md-start text-sm-center text-center">
-							<?php dynamic_sidebar( 'footer-1' ); ?>
-						</div>
-						<div class="<?php if ( is_active_sidebar('footer-2') == '' ) { echo 'footer_hide'; } else { echo esc_html( $colmd ); } ?> text-md-start text-sm-center text-center">
-							<?php dynamic_sidebar( 'footer-2' ); ?>
-						</div>
-						<div class="<?php if ( is_active_sidebar('footer-3') == '' ) { echo 'footer_hide'; } else { echo esc_html( $colmd ); } ?> text-md-start text-sm-center text-center">
-							<?php dynamic_sidebar( 'footer-3' ); ?>
-					</div>
-				</div>
-			</div>
-		</div>
-	<?php get_template_part( 'template-parts/footer/copyright' ); ?>
-	</div>
-</div>
-<!-- </section> -->
-<?php } ?>
+<?php if (get_theme_mod('vw_tourism_pro_custom_footer_widget_section', 'true') != ''): ?>
+    <div id="footer-inner" style="<?php echo esc_attr($footer_backg); ?>" class="<?php echo esc_attr($img_bg); ?>">
+        <div class="container footer-cols">
+            <div class="row">
+                <!-- 左カラム（連絡先情報） -->
+                <div class="col-lg-4 col-md-12">
+                    <?php get_template_part('template-parts/footer/footer-contact'); ?>
+                </div>
+
+                <!-- 右カラム（ウィジェットエリア） -->
+                <div class="col-lg-8 col-md-12">
+                    <div class="row footer-col-div py-xl-5 py-lg-3">
+                        <?php foreach ($sidebars as $sidebar): ?>
+                            <?php if (is_active_sidebar($sidebar)): ?>
+                                <div class="<?php echo esc_attr($col_class); ?> text-md-start text-center">
+                                    <?php dynamic_sidebar($sidebar); ?>
+                                </div>
+                            <?php endif; ?>
+                        <?php endforeach; ?>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <?php get_template_part('template-parts/footer/copyright'); ?>
+    </div>
+<?php endif; ?>
