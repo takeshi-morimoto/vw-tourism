5125d60ac7c5862cebf47215a6cf51e56053fc77
Takeshi Morimoto <takeshi.morimoto@koikoi.co.jp>
2024-12-19 17:56:53 +0900

    test

--- template-parts/home/section-explore.php
+++ template-parts/home/section-explore.php
@@ -22,202 +22,137 @@ $query = new WP_Query($args);
 $default_image = 'https://example.com/default-image.jpg';
 ?>
 
-<section id="explore" style="<?php echo esc_attr($explore_bg); ?>">
-    <div class="container">
-        <div class="row align-items-center">
-            <!-- 左カラム -->
-            <div class="col-lg-6">
-                <p class="sec-sub-heading">Explore</p>
-                <h2 class="sec-heading">Discover Japan</h2>
-                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
-
-                <!-- カスタムセレクト -->
-                <div class="custom-select-wrapper">
-                    <div class="custom-select">
-                        <span class="custom-select-trigger">Select Region</span>
-                        <ul class="custom-options">
-                            <?php if ($query->have_posts()): ?>
-                                <?php while ($query->have_posts()): $query->the_post(); ?>
-                                    <li class="custom-option" data-post-id="<?php echo get_the_ID(); ?>">
-                                        <?php echo get_the_title(); ?>
-                                    </li>
-                                <?php endwhile; ?>
-                                <?php wp_reset_postdata(); ?>
-                            <?php else: ?>
-                                <li class="custom-option">No Regions Available</li>
-                            <?php endif; ?>
-                        </ul>
-                    </div>
-                </div>
-
-                <!-- スライダー -->
-                <div class="explore-main-wrapper mt-2">
-                    <div class="owl-carousel explore-slider">
-                        <!-- Ajaxでスライダー要素を挿入 -->
-                    </div>
-                </div>
+<section id="explore" class="pb-0">
+  <div class="container">
+    <div class="row align-items-center">
+      <!-- 左カラム（テキストとカスタムセレクト） -->
+      <div class="col-lg-6">
+        <p class="sec-sub-heading text-md-start text-center">Explore</p>
+        <h2 class="sec-heading text-md-start text-center">France Discover</h2>
+        <p class="text-md-start text-center exp-para">
+          Lorem Ipsum is simply dummy text of the printing and typesetting industry...
+        </p>
+        
+        <div class="explore-inner">
+          <div class="custom-select-wrapper">
+            <div class="custom-select">
+              <span class="custom-select-trigger explore-select-title" data-value="123">Ile De France 1</span>
+              <ul class="custom-options" style="display: none;">
+                <li class="custom-option" data-value="125">Ile De France 3</li>
+                <li class="custom-option" data-value="124">Ile De France 2</li>
+                <li class="custom-option" data-value="123">Ile De France 1</li>
+                <li class="custom-option" data-value="122">Ile De France</li>
+              </ul>
             </div>
-
-            <!-- 右カラム：地図画像 -->
-            <div class="col-lg-6">
-                <img src="<?php echo esc_url(get_theme_mod('vw_tourism_pro_explore_map_img', $default_image)); ?>" alt="Explore Map">
-            </div>
-        </div>
-    </div>
-</section>
-
-<section id="explore" style="<?php echo esc_attr($explore_bg); ?>">
-    <div class="container">
-        <div class="row align-items-center">
-            <!-- 左カラム -->
-            <div class="col-lg-6">
-                <p class="sec-sub-heading">Explore</p>
-                <h2 class="sec-heading">Discover Japan</h2>
-                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
-
-                <!-- カスタムセレクト -->
-                <div class="custom-select-wrapper">
-                    <div class="custom-select">
-                        <span class="custom-select-trigger">Select Region</span>
-                        <ul class="custom-options">
-                            <?php if ($query->have_posts()): ?>
-                                <?php while ($query->have_posts()): $query->the_post(); ?>
-                                    <li class="custom-option" data-post-id="<?php echo get_the_ID(); ?>">
-                                        <?php echo get_the_title(); ?>
-                                    </li>
-                                <?php endwhile; ?>
-                                <?php wp_reset_postdata(); ?>
-                            <?php else: ?>
-                                <li class="custom-option">No Regions Available</li>
-                            <?php endif; ?>
-                        </ul>
-                    </div>
-                </div>
-
-                <!-- スライダー -->
-                <div class="explore-main-wrapper mt-2">
-                    <!-- Slick用のラッパ -->
-                    <div class="explore-slider">
-                        <!-- Ajaxでスライダー要素を挿入 -->
-                    </div>
-                </div>
-            </div>
-
-            <!-- 右カラム：地図画像 -->
-            <div class="col-lg-6">
-                <img src="<?php echo esc_url(get_theme_mod('vw_tourism_pro_explore_map_img', $default_image)); ?>" alt="Explore Map">
-            </div>
-        </div>
-    </div>
-</section>
-
-<script>
-jQuery(document).ready(function ($) {
-    const selectTrigger = $('.custom-select-trigger');
-    const selectOptions = $('.custom-options');
-    const slider = $('.explore-slider');
-
-    // ドロップダウン開閉
-    selectTrigger.on('click', function () {
-        selectOptions.toggleClass('open');
-    });
-
-    // オプション選択時の処理
-    selectOptions.on('click', '.custom-option', function () {
-        const selectedOption = $(this).text(); // 選択したテキストを取得
-        const postId = $(this).data('post-id'); // 選択したオプションの投稿IDを取得
-
-        // トリガーテキストを更新
-        selectTrigger.text(selectedOption);
-
-        // ドロップダウンを閉じる
-        selectOptions.removeClass('open');
-
-        // スライダーを更新
-        updateSlider(postId);
-    });
-
-    // スライダーを更新する関数
-    function updateSlider(postId) {
-        $.ajax({
-            url: ajax_object.ajaxurl,
-            method: 'POST',
-            data: {
-                action: 'get_explore_meta_fields',
-                post_id: postId,
-                nonce: ajax_object.nonce,
-            },
-            success: function (response) {
-                if (response.success) {
-                    populateSlider(response.meta_fields);
-                } else {
-                    console.error('AJAX Error:', response.message);
-                }
-            },
-            error: function (error) {
-                console.error('Fetch Error:', error);
-            },
-        });
-    }
-
-    // スライダーのアイテムを生成
-    function populateSlider(items = []) {
-        slider.empty(); // スライダーの初期化
-
-        items.forEach(function (item) {
-            const slide = `
+          </div>
+          
+          <div class="explore-main-wrapper mt-2">
+            <p class="text-md-start text-center">
+              Lorem Ipsum is simply dummy text of the printing and typesetting industry...
+            </p>
+            <!-- Slickスライダー用コンテナ -->
+            <div class="slider">
+              <!-- スライド1 -->
+              <div class="slide-item">
                 <div class="explore-inners">
-                    <div class="explore-img">
-                        <img src="${item.image}" alt="${item.text1}">
+                  <div class="explore-img">
+                    <img style="border-radius:10px;" 
+                         src="https://preview.vwthemesdemo.com/vw-tourism/wp-content/themes/vw-tourism-pro/assets/images/explore/explore2-1.png" 
+                         alt="Slide 1">
+                  </div>
+                  <div class="d-flex gap-2 mt-2">
+                    <div class="explore-inner-box">
+                      <h6 class="explore-inner-title">875Km</h6>
+                      <h6 class="explore-inner-title">Area</h6>
                     </div>
-                    <div class="d-flex gap-2 mt-2">
-                        <div class="explore-inner-box">
-                            <h6 class="explore-inner-title">${item.text1}</h6>
-                            <h6 class="explore-inner-title">${item.text2}</h6>
-                        </div>
+                    <div class="explore-inner-box">
+                      <h6 class="explore-inner-title">91,12,520</h6>
+                      <h6 class="explore-inner-title">Population</h6>
                     </div>
+                  </div>
                 </div>
-            `;
-            slider.append(slide);
-        });
+              </div>
+              <!-- スライド2 -->
+              <div class="slide-item">
+                <div class="explore-inners">
+                  <div class="explore-img">
+                    <img style="border-radius:10px;" 
+                         src="https://preview.vwthemesdemo.com/vw-tourism/wp-content/themes/vw-tourism-pro/assets/images/explore/explore2-2.png" 
+                         alt="Slide 2">
+                  </div>
+                  <div class="d-flex gap-2 mt-2">
+                    <div class="explore-inner-box">
+                      <h6 class="explore-inner-title">85.09%</h6>
+                      <h6 class="explore-inner-title">Literacy</h6>
+                    </div>
+                    <div class="explore-inner-box">
+                      <h6 class="explore-inner-title">123456</h6>
+                      <h6 class="explore-inner-title">Pincode</h6>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <!-- スライド3 -->
+              <div class="slide-item">
+                <div class="explore-inners">
+                  <div class="explore-img">
+                    <img style="border-radius:10px;" 
+                         src="https://preview.vwthemesdemo.com/vw-tourism/wp-content/themes/vw-tourism-pro/assets/images/explore/explore2-3.png" 
+                         alt="Slide 3">
+                  </div>
+                  <div class="d-flex gap-2 mt-2">
+                    <div class="explore-inner-box">
+                      <h6 class="explore-inner-title">65+</h6>
+                      <h6 class="explore-inner-title">Villages</h6>
+                    </div>
+                    <div class="explore-inner-box">
+                      <h6 class="explore-inner-title">French</h6>
+                      <h6 class="explore-inner-title">Language</h6>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <!-- 必要に応じてスライド追加 -->
+            </div>
+          </div>
+        </div>
+      </div>
+      
+      <!-- 右カラム（地図画像） -->
+      <div class="col-lg-6">
+        <img class="map-img"
+             src="https://preview.vwthemesdemo.com/vw-tourism/wp-content/themes/vw-tourism-pro/assets/images/explore/map.png" 
+             alt="Map">
+      </div>
+    </div>
+  </div>
+</section>
 
-        initializeSlick(); // Slick初期化
-    }
-
-    // Slickを初期化する関数
-    function initializeSlick() {
-        // すでに初期化されていたらdestroy
-        if (slider.hasClass('slick-initialized')) {
-            slider.slick('unslick');
+<!-- Slick初期化用スクリプト -->
+<script>
+jQuery(document).ready(function($){
+  $('.slider').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    arrows: true,
+    dots: true,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    responsive: [
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 2
         }
-
-        slider.slick({
-            infinite: true,
-            slidesToShow: 3,
-            slidesToScroll: 1,
-            autoplay: true,
-            autoplaySpeed: 5000,
-            dots: true,
-            arrows: true,
-            responsive: [
-                {
-                    breakpoint: 992,
-                    settings: {
-                        slidesToShow: 3
-                    }
-                },
-                {
-                    breakpoint: 576,
-                    settings: {
-                        slidesToShow: 1
-                    }
-                }
-            ]
-        });
-    }
-
-    // ページ読み込み時にSlickを初期化（初期にアイテムがない場合は空スライダーになる）
-    initializeSlick();
+      },{
+        breakpoint: 576,
+        settings: {
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
 });
 </script>
 
