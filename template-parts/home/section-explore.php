01313d57e0623bc1243bb48a5a69f924c766d5e7
Takeshi Morimoto <takeshi.morimoto@koikoi.co.jp>
2024-12-19 19:39:53 +0900

    テスト

--- template-parts/home/section-explore.php
+++ template-parts/home/section-explore.php
@@ -11,7 +11,7 @@ if ($bg_color = get_theme_mod('vw_tourism_pro_explore_bgcolor', '')) {
     $explore_bg = 'background-image:url(' . esc_url($bg_image) . ');';
 }
 
-// WP_Queryでカスタム投稿タイプ「tcp_explore」を取得
+// カスタム投稿タイプ「tcp_explore」を取得
 $args = [
     'post_type' => 'tcp_explore',
     'posts_per_page' => 10,
@@ -25,160 +25,100 @@ $default_image = 'https://example.com/default-image.jpg';
 <section id="explore" class="pb-0" style="<?php echo esc_attr($explore_bg); ?>">
   <div class="container">
     <div class="row align-items-center">
-      <div class="col-lg-6">
-        <p class="sec-sub-heading text-md-start text-center">Explore</p>
-        <h2 class="sec-heading text-md-start text-center">France Discover</h2>
-        <p class="text-md-start text-center exp-para">
-          Lorem Ipsum is simply dummy text of the printing and typesetting industry...
-        </p>
-        
+      <div class="col-lg-6 wow zoomIn delay-2000">
+        <?php if(get_theme_mod('vw_tourism_pro_explore_sub_heading')!=''){ ?>
+          <p class="sec-sub-heading text-md-start text-center"><?php echo esc_html(get_theme_mod('vw_tourism_pro_explore_sub_heading')); ?></p>
+        <?php } ?>
+        <?php if(get_theme_mod('vw_tourism_pro_explore_heading')!=''){ ?>
+          <h2 class="sec-heading text-md-start text-center"><?php echo esc_html(get_theme_mod('vw_tourism_pro_explore_heading')); ?></h2>
+        <?php } ?>
+        <?php if(get_theme_mod('vw_tourism_pro_explore_paragraph')!=''){ ?>
+          <p class="text-md-start text-center exp-para"><?php echo esc_html(get_theme_mod('vw_tourism_pro_explore_paragraph')); ?></p>
+        <?php } ?>
+
         <div class="explore-inner">
-          <div class="custom-select-wrapper">
-            <div class="custom-select">
-              <span class="custom-select-trigger explore-select-title" data-value="123">Ile De France 1</span>
-              <ul class="custom-options" style="display: none;">
-                <li class="custom-option" data-value="125">Ile De France 3</li>
-                <li class="custom-option" data-value="124">Ile De France 2</li>
-                <li class="custom-option" data-value="123">Ile De France 1</li>
-                <li class="custom-option" data-value="122">Ile De France</li>
-              </ul>
-            </div>
-          </div>
-          
+          <?php
+            if ($query->have_posts()) {
+              $first_post = true;
+              $selected_post_title = '';
+              $selected_post_id = '';
+              ?>
+              <div class="custom-select-wrapper">
+                <div class="custom-select">
+                  <!-- 最初の投稿をデフォルト選択状態に -->
+                  <?php 
+                    // ループ一旦回して最初の投稿情報を取得
+                    $query->the_post();
+                    $selected_post_id = get_the_ID();
+                    $selected_post_title = get_the_title();
+                    wp_reset_postdata(); // 再度ループ走らせるためにリセット
+
+                    // 再ループ開始
+                    $query->rewind_posts();
+                  ?>
+                  <span class="custom-select-trigger explore-select-title" data-value="<?php echo esc_attr($selected_post_id); ?>">
+                    <?php echo esc_html($selected_post_title); ?> <i class="fa-solid fa-chevron-down"></i>
+                  </span>
+                  <ul class="custom-options" style="display: none;">
+                    <?php
+                      while ($query->have_posts()) {
+                        $query->the_post();
+                        $post_id = get_the_ID();
+                        $post_title = get_the_title();
+                        ?>
+                        <li class="custom-option" data-value="<?php echo esc_attr($post_id); ?>"><?php echo esc_html($post_title); ?></li>
+                        <?php
+                      }
+                      wp_reset_postdata();
+                    ?>
+                  </ul>
+                </div>
+              </div>
+            <?php } else {
+              // 投稿がない場合
+              ?>
+              <p>No Regions Available</p>
+              <?php
+            }
+          ?>
+
           <div class="explore-main-wrapper mt-2">
+            <!-- ここに説明文やAjaxで動的更新するテキストなどを配置 -->
             <p class="text-md-start text-center">
+              <!-- この部分にロジックを追加して、選択された投稿IDに応じた説明をAjaxなどで取得可能 -->
+              <!-- 例：選択された投稿IDを用いてget_post_meta()などで情報取得 -->
               Lorem Ipsum is simply dummy text of the printing and typesetting industry...
             </p>
-            <div class="slider">
-              <div class="slide-item">
-                <div class="explore-inners">
-                  <div class="explore-img">
-                    <img style="border-radius:10px;" 
-                         src="https://preview.vwthemesdemo.com/vw-tourism/wp-content/themes/vw-tourism-pro/assets/images/explore/explore2-1.png" 
-                         alt="Slide 1">
+
+            <!-- スライダー要素（OwlかSlickかは状況に応じて） -->
+            <!-- Owl Carousel例 -->
+            <div class="owl-carousel owl-loaded owl-drag">
+              <!-- ここに投稿情報から抽出したスライドアイテムを表示することも可能 -->
+              <!-- 例として固定アイテムを配置 -->
+              <div class="explore-inners">
+                <div class="explore-img">
+                  <img style="border-radius: 10px;" src="https://example.com/image1.jpg" alt="Slide 1">
+                </div>
+                <div class="d-flex gap-2 mt-2">
+                  <div class="explore-inner-box">
+                    <h6 class="explore-inner-title">875Km</h6>
+                    <h6 class="explore-inner-title">Area</h6>
                   </div>
-                  <div class="d-flex gap-2 mt-2">
-                    <div class="explore-inner-box">
-                      <h6 class="explore-inner-title">875Km</h6>
-                      <h6 class="explore-inner-title">Area</h6>
-                    </div>
-                    <div class="explore-inner-box">
-                      <h6 class="explore-inner-title">91,12,520</h6>
-                      <h6 class="explore-inner-title">Population</h6>
-                    </div>
+                  <div class="explore-inner-box">
+                    <h6 class="explore-inner-title">91,12,520</h6>
+                    <h6 class="explore-inner-title">Population</h6>
                   </div>
                 </div>
               </div>
-              <div class="slide-item">
-                <div class="explore-inners">
-                  <div class="explore-img">
-                    <img style="border-radius:10px;" 
-                         src="https://preview.vwthemesdemo.com/vw-tourism/wp-content/themes/vw-tourism-pro/assets/images/explore/explore2-2.png" 
-                         alt="Slide 2">
-                  </div>
-                  <div class="d-flex gap-2 mt-2">
-                    <div class="explore-inner-box">
-                      <h6 class="explore-inner-title">85.09%</h6>
-                      <h6 class="explore-inner-title">Literacy</h6>
-                    </div>
-                    <div class="explore-inner-box">
-                      <h6 class="explore-inner-title">123456</h6>
-                      <h6 class="explore-inner-title">Pincode</h6>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div class="slide-item">
-                <div class="explore-inners">
-                  <div class="explore-img">
-                    <img style="border-radius:10px;" 
-                         src="https://preview.vwthemesdemo.com/vw-tourism/wp-content/themes/vw-tourism-pro/assets/images/explore/explore2-3.png" 
-                         alt="Slide 3">
-                  </div>
-                  <div class="d-flex gap-2 mt-2">
-                    <div class="explore-inner-box">
-                      <h6 class="explore-inner-title">65+</h6>
-                      <h6 class="explore-inner-title">Villages</h6>
-                    </div>
-                    <div class="explore-inner-box">
-                      <h6 class="explore-inner-title">French</h6>
-                      <h6 class="explore-inner-title">Language</h6>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <!-- 必要に応じて他のスライドを追加 -->
+              <!-- 必要に応じて他のスライドアイテムを追加 -->
             </div>
           </div>
         </div>
       </div>
       
-      <div class="col-lg-6">
-        <img class="map-img"
-             src="https://preview.vwthemesdemo.com/vw-tourism/wp-content/themes/vw-tourism-pro/assets/images/explore/map.png" 
-             alt="Map">
+      <div class="col-lg-6 wow zoomIn delay-2000">
+        <img class="map-img" src="<?php echo esc_html(get_theme_mod('vw_tourism_pro_explore_map_img')); ?>" alt="">
       </div>
     </div>
   </div>
 </section>
-
-<script>
-jQuery(window).on('load', function(){
-  var $slider = jQuery('.slider');
-  if ($slider.length > 0) {
-    console.log('Slider element found:', $slider);
-    if (typeof jQuery.fn.slick !== 'undefined') {
-      // ここでslick()を初期化
-      $slider.slick({
-        infinite: true,
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        arrows: true,
-        dots: true,
-        autoplay: true,
-        autoplaySpeed: 4000,
-        accessibility: false, // 追加
-        responsive: [
-          {
-            breakpoint: 992,
-            settings: {
-              slidesToShow: 2
-            }
-          },{
-            breakpoint: 576,
-            settings: {
-              slidesToShow: 1
-            }
-          }
-        ]
-      });
-    } else {
-      console.error('Slick is not loaded. Check if slick.min.js is enqueued properly.');
-    }
-  } else {
-    console.error('.slider element not found.');
-  }
-});
-</script>
-
-<?php
-// デバッグ用コード（開発中のみ有効にする）
-if (defined('WP_DEBUG') && WP_DEBUG) {
-    echo '<h3>テスト: 投稿データ</h3>';
-    if ($query->have_posts()):
-        while ($query->have_posts()): $query->the_post();
-            echo '<p>タイトル: ' . get_the_title() . '</p>';
-            $additional_fields = get_post_meta(get_the_ID(), 'package_explore_meta_fields', true);
-            if (!empty($additional_fields)) {
-                echo '<pre>';
-                print_r($additional_fields);
-                echo '</pre>';
-            } else {
-                echo '<p>追加メタフィールドが見つかりません</p>';
-            }
-        endwhile;
-        wp_reset_postdata();
-    else:
-        echo '<p>投稿が見つかりませんでした。</p>';
-    endif;
-}
