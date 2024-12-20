c7a5bb9eb1975c33d1e19311b53f708ac3bb81a4
takeshi-morimoto <takeshi.morimoto@koikoi.co.jp>
2024-12-20 16:25:12 +0900

    test

--- template-parts/header/content-header.php
+++ template-parts/header/content-header.php
@@ -1,8 +1,7 @@
 <div class="container">
-  <div class="row justify-content-between bg-media align-items-center">
-    
+  <div class="row justify-content-between align-items-center bg-media">
     <!-- ロゴセクション -->
-    <div class="logo col-lg-2 col-md-4 col-4 order-lg-1">
+    <div class="logo col-lg-2 col-md-4 col-4">
       <?php if (has_custom_logo()) {  
           vw_tourism_pro_the_custom_logo();
       } else { ?>
@@ -12,26 +11,20 @@
       <?php } ?>
     </div>
 
-    <!-- メニューナビゲーション -->
-    <div class="header-menu col-lg-10 col-md-12 col-12 order-lg-2">
-      <div class="menubar">
-        <div class="toggle-nav mobile-menu">
-          <!-- ハンバーガーメニュー -->
-          <button id="open_nav" aria-expanded="false" aria-label="Open Menu">
-              <i class="<?php echo esc_html(get_theme_mod('vw_tourism_pro_res_open_menu_icon', 'fas fa-bars')); ?>"></i>
-          </button>
-        </div>
-        <nav id="site-navigation" class="main-navigation">
-          <?php wp_nav_menu(array(
-            'theme_location' => 'primary',
-            'container_class' => 'menu clearfix',
-            'menu_class' => 'clearfix',
-            'fallback_cb' => 'wp_page_menu',
-          )); ?>
-        </nav>
-      </div>
+    <!-- ハンバーガーメニュー -->
+    <div class="hamburger-menu col-lg-2 col-md-2 col-2 text-end">
+      <button id="open_nav" aria-expanded="false" aria-label="Open Menu">
+          <i class="<?php echo esc_html(get_theme_mod('vw_tourism_pro_res_open_menu_icon', 'fas fa-bars')); ?>"></i>
+      </button>
     </div>
-
   </div>
-  <span class="d-none" id="menu-width"><?php echo esc_html($menu_width); ?></span>
 </div>
+
+<nav id="site-navigation" class="main-navigation">
+  <?php wp_nav_menu(array(
+    'theme_location' => 'primary',
+    'container_class' => 'menu clearfix',
+    'menu_class' => 'clearfix',
+    'fallback_cb' => 'wp_page_menu',
+  )); ?>
+</nav>
