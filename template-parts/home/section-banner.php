b79a6265494ce52ea8bb09b644fad502d994d848
Takeshi Morimoto <takeshi.morimoto@koikoi.co.jp>
2025-01-04 16:01:01 +0900

    アニメーションを修正

--- template-parts/home/section-banner.php
+++ template-parts/home/section-banner.php
@@ -57,11 +57,9 @@ if ('Disable' == $section_hide) {
                       </div>
                   <?php endif; ?>
               <?php endfor; ?>
-
               </div>
           </div>
       </div>
-
 
 <!-- desktop svg -->
 <svg class="position-absolute banner-svg d-md-block d-none" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"  width="1820.479" height="780"  viewBox="0 0 1790 835.763">
