a2a498b6d7a168675187fe4b875a4ee89820b317
takeshi-morimoto <takeshi.morimoto@koikoi.co.jp>
2025-01-16 23:59:42 +0900

    test

--- functions.php
+++ functions.php
@@ -830,7 +830,6 @@ add_action('plugins_loaded', function () {
 });
 
 add_action('init', function () {
-    // 重複登録を防ぐためのフック確認
     static $filter_registered = false;
     if ($filter_registered) {
         return;
@@ -843,20 +842,17 @@ add_action('init', function () {
         $tags['meeting_location'] = [
             'description' => __('Meeting Location for the booking', 'motopress-appointment'),
             'callback'    => function ($booking) {
-                // $booking オブジェクトの検証
                 if (!$booking || !is_object($booking)) {
                     error_log('Invalid booking object.');
                     return __('Invalid booking object', 'motopress-appointment');
                 }
 
-                // サービスIDの取得
                 $service_id = $booking->getServiceId();
                 if (!$service_id) {
                     error_log('Service ID is not set or invalid.');
                     return __('Service ID not found', 'motopress-appointment');
                 }
 
-                // meeting_location フィールドの取得
                 $meeting_location = get_field('meeting_location', $service_id);
                 error_log('Meeting Location: ' . ($meeting_location ? $meeting_location : 'Not set'));
 
